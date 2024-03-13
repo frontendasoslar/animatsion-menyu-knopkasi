d2b9ba4eab21a8d37721bad200009d574616351c
frondendasoslar <faxriddin19951021@gmai.com>
2024-03-13 13:37:34 +0500

    add code info

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
-const elMenubtn = document.querySelector('.menu-btn');
-const elSpan = document.querySelector('.span');
+const elMenubtn = document.querySelector('.menu-btn');//link klasi tobib olinayapdi
+const elSpan = document.querySelector('.span');//span klasi topib ilinayapdi
 
-elMenubtn.addEventListener('click', function() {
-  elSpan.classList.toggle('span-btn');
+
+//link bosilganda spanning klasiga yangi klas qo'shish uchun kiritilayapdi
+elMenubtn.addEventListener('click', function() {//link klasi click hodisasiga quloq solayapdi va funktsiya kiritilayapdi
+  elSpan.classList.toggle('span-btn');//link bosilganda spanning klasiga yangi klas qo'shilayapdi
 })
